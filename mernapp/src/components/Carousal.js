b51fa36a3b29572e933d1433b968243ea018f1db
Nmsk-sriram <167607313+Nmsk-sriram@users.noreply.github.com>
2024-10-20 14:08:15 +0530

    Add files via upload

---
+++ mernapp/src/components/Carousal.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Carousel() {
+    return (
+        <div>
+
+            <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel" style={{objectFit:"contain !important"}}>
+
+                <div className="carousel-inner " id='carousel'>
+                    <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+                        <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                            <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
+                            <button className="btn text-white bg-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div className="carousel-item active" >
+                        <img src="https://static.vecteezy.com/system/resources/previews/003/690/403/large_2x/food-delivery-online-on-phone-orange-background-illustration-fastfood-design-concept-business-shop-promotion-transport-to-home-free-vector.jpg" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-food-delivery-logo-design-png-image_5392526.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://wallpaperaccess.com/full/6221183.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+
+        </div>
+    )
+}
